61074ba8aa9fe4dd62d8ac2d3971dc79372986d9
Connor Fahy <heskey333@gmail.com>
2016-10-14 11:59:54 -0400

    Added the 'bird' (which is actually just a particle effect)

---
+++ Assets/Bird.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+	/// <summary>
+	/// Follows the mouse and destroys any object with a destroyable component. 
+	/// </summary>
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit info;
+		Physics.Raycast(Camera.current.ScreenPointToRay(Input.mousePosition), out info);
+		//print(Camera.current + "  " + Input.mousePosition + "  " + info);
+		transform.position = info.point;
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		print("overlapping" + col.gameObject);
+		if (col.GetComponent<Destroyable>())
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
