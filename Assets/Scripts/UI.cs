9d0491ff67b3b7b1a9426b78e3ce4179df55f85f
John Steigerwald <johnstiggy@gmail.com>
2016-10-16 16:03:34 -0400

    Added UI prefab

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+
+	public void LoadScalable ()
+    {
+        SceneManager.LoadScene(0);
+	}
+	
+	public void LoadEmergent ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadTwoLevelFormation()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
